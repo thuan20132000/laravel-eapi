bc2b4e4a316c3e60b72e4042cb3068217a8aa694
thuantruong <thuan20132000@gmail.com>
2020-03-27 21:54:07 +0700

    Modify Product in ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ? "Out of Stock": $this->stock,
+            'discount'=>$this->discount,
+            'totalPrice'=>$this->price - ($this->price * $this->discount)/100,
+            'rating'=>$this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : "No rating yet",
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id)
+            ]
+
         ];
     }
 }
